8a8dbc0798738a65644f235a4fa5e5023b035cfd
Robert Va?an <Robert Va?an@localhost>
2014-07-01 01:05:38 +0200

    Binding-friendly custom control derived from ItemsControl

---
+++ ControlJungle/PropertySheet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ControlJungle
+{
+    public class PropertySheet : ItemsControl
+    {
+        static PropertySheet()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(PropertySheet), new FrameworkPropertyMetadata(typeof(PropertySheet)));
+        }
+    }
+}
---
+++ TestApp/PropertySheetSampleViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UpdateControls.Fields;
+
+namespace TestApp
+{
+    class PropertySheetSampleViewModel
+    {
+        readonly Independent<string> InputValue = new Independent<string>("my input");
+
+        public string Input { get { return InputValue.Value; } set { InputValue.Value = value; } }
+    }
+}
