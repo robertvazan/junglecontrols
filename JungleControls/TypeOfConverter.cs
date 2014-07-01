8997c1c8e4da44da0ec38a5346ed365db78bbe48
Robert Vazan <robvcode@gmail.com>
2014-07-01 05:21:02 +0200

    Switch templates via TypeOfConverter

---
+++ JungleControls/TypeOfConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace JungleControls
+{
+    public class TypeOfConverter : IValueConverter
+    {
+        public static readonly TypeOfConverter Instance = new TypeOfConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) { return (value == null) ? null : value.GetType(); }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) { throw new NotImplementedException(); }
+    }
+}
