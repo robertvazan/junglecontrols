0b51ea827d2ee3e1c05cf3b34b7478ae864cc115
Robert Vazan <robvcode@gmail.com>
2014-07-01 04:08:25 +0200

    Allow other children than PropertySheetRow

--- ControlJungle/PropertySheet.cs
+++ ControlJungle/PropertySheet.cs
@@ -10,6 +10,10 @@ namespace ControlJungle
 {
     public class PropertySheet : ItemsControl
     {
+        public static readonly DependencyProperty HeaderProperty = DependencyProperty.RegisterAttached("Header", typeof(object), typeof(PropertySheet));
+        public static void SetHeader(DependencyObject target, object value) { target.SetValue(HeaderProperty, value); }
+        public static object GetHeader(DependencyObject target) { return target.GetValue(HeaderProperty); }
+
         static PropertySheet()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(PropertySheet), new FrameworkPropertyMetadata(typeof(PropertySheet)));
