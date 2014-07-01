0b8e7e9eab8e8690cfd36bb2bed394478eb90afa
Robert Va?an <Robert Va?an@localhost>
2014-07-01 04:15:47 +0200

    Renamed the project

--- JungleControls/PropertySheetRow.cs
+++ JungleControls/PropertySheetRow.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 
-namespace ControlJungle
+namespace JungleControls
 {
     public class PropertySheetRow : HeaderedContentControl
     {
