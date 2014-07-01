5019ed0d781d2671275f78290516aa579cccd5f4
Robert Va?an <Robert Va?an@localhost>
2014-07-01 22:49:53 +0200

    Implemented DataPresenter

---
+++ JungleControls/DataPresenter.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JungleControls
+{
+    public partial class DataPresenter : UserControl
+    {
+        public static readonly DependencyProperty InnerProperty = DependencyProperty.Register("Inner", typeof(object), typeof(DataPresenter));
+        public object Inner { get { return GetValue(InnerProperty); } set { SetValue(InnerProperty, value); } }
+
+        public static readonly DependencyProperty InnerDataContextProperty = DependencyProperty.Register("InnerDataContext", typeof(object), typeof(DataPresenter));
+        public object InnerDataContext { get { return GetValue(InnerDataContextProperty); } set { SetValue(InnerDataContextProperty, value); } }
+
+        public DataPresenter()
+        {
+            InitializeComponent();
+        }
+    }
+}
