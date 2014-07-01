bf15fc44c712f6b6fed3cc2413c9f5887e985351
Robert Vazan <robvcode@gmail.com>
2014-07-01 21:53:10 +0200

    Basic UpdateControls-based implementation

---
+++ JungleControls/PropertySheetRowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using UpdateControls;
+using UpdateControls.Fields;
+
+namespace JungleControls
+{
+    class PropertySheetRowViewModel
+    {
+        readonly FrameworkElement Element;
+        readonly Independent<string> HeaderIndependent = new Independent<string>();
+
+        public string Header { get { return HeaderIndependent.Value; } }
+        public FrameworkElement Content { get { return Element; } }
+
+        public PropertySheetRowViewModel(FrameworkElement element)
+        {
+            Element = element;
+            ControlFacade.Lift(element, PropertySheet.HeaderProperty, HeaderIndependent);
+        }
+    }
+}
