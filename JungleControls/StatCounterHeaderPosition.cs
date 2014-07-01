4b7e2fb492cb066dd00213c7621427bd9f19dd02
Robert Va?an <Robert Va?an@localhost>
2014-07-02 01:34:42 +0200

    Basic StatCounter control

---
+++ JungleControls/StatCounterHeaderPosition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JungleControls
+{
+    public enum StatCounterHeaderPosition
+    {
+        Top,
+        Bottom
+    }
+}
