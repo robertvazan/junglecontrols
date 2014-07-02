7e8dba90eedc81226cc8594b6786a255a6b258fb
Robert Va?an <Robert Va?an@localhost>
2014-07-02 14:22:18 +0200

    NuGet release

--- JungleControls/Properties/AssemblyInfo.cs
+++ JungleControls/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ControlJungle")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("JungleControls")]
+[assembly: AssemblyDescription("Free WPF controls for your app")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ControlJungle")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Robert Važan")]
+[assembly: AssemblyProduct("JungleControls")]
+[assembly: AssemblyCopyright("Copyright © 2014 Robert Važan")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,4 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
