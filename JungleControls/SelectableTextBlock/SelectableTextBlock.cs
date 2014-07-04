df163a7a144f0756bd571136f012579279cab3ea
Robert Va?an <Robert Va?an@localhost>
2014-07-04 18:10:53 +0200

    Basic SelectableTextBlock

---
+++ JungleControls/SelectableTextBlock/SelectableTextBlock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace JungleControls
+{
+    public class SelectableTextBlock : Control
+    {
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(SelectableTextBlock), new FrameworkPropertyMetadata(""));
+        public string Text { get { return (string)GetValue(TextProperty); } set { SetValue(TextProperty, value); } }
+
+        static SelectableTextBlock() { ControlFacade.InitializeFacade<SelectableTextBlock>(); }
+        public override void OnApplyTemplate() { ControlFacade.TemplateFacade<SelectableTextBlockView>(GetTemplateChild("InternalPresenter"), this); }
+    }
+}
