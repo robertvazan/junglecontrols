34e73fc12f91ce6bfd8777dfdffcd672650b1085
Robert Va?an <Robert Va?an@localhost>
2014-07-04 19:00:53 +0200

    SelectableTextBlock properties

--- JungleControls/SelectableTextBlock/SelectableTextBlock.cs
+++ JungleControls/SelectableTextBlock/SelectableTextBlock.cs
@@ -14,7 +14,28 @@ namespace JungleControls
         public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(SelectableTextBlock), new FrameworkPropertyMetadata(""));
         public string Text { get { return (string)GetValue(TextProperty); } set { SetValue(TextProperty, value); } }
 
-        static SelectableTextBlock() { ControlFacade.InitializeFacade<SelectableTextBlock>(); }
-        public override void OnApplyTemplate() { ControlFacade.TemplateFacade<SelectableTextBlockView>(GetTemplateChild("InternalPresenter"), this); }
+        public static readonly DependencyProperty TextAlignmentProperty = DependencyProperty.Register("TextAlignment", typeof(TextAlignment), typeof(SelectableTextBlock));
+        public TextAlignment TextAlignment { get { return (TextAlignment)GetValue(TextAlignmentProperty); } set { SetValue(TextAlignmentProperty, value); } }
+
+        public static readonly DependencyProperty TextDecorationsProperty = DependencyProperty.Register("TextDecorations", typeof(TextDecorationCollection), typeof(SelectableTextBlock));
+        public TextDecorationCollection TextDecorations { get { return (TextDecorationCollection)GetValue(TextDecorationsProperty); } set { SetValue(TextDecorationsProperty, value); } }
+
+        public static readonly DependencyProperty TextWrappingProperty = DependencyProperty.Register("TextWrapping", typeof(TextWrapping), typeof(SelectableTextBlock), new FrameworkPropertyMetadata(TextWrapping.NoWrap));
+        public TextWrapping TextWrapping { get { return (TextWrapping)GetValue(TextWrappingProperty); } set { SetValue(TextWrappingProperty, value); } }
+
+        static SelectableTextBlock()
+        {
+            ControlFacade.InitializeFacade<SelectableTextBlock>();
+        }
+
+        public SelectableTextBlock()
+        {
+            TextDecorations = new TextDecorationCollection();
+        }
+
+        public override void OnApplyTemplate()
+        {
+            ControlFacade.TemplateFacade<SelectableTextBlockView>(GetTemplateChild("InternalPresenter"), this);
+        }
     }
 }
