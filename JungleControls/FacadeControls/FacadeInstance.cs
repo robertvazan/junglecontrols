bc93659a3993837df41e25594813578e7fb64600
Robert Vazan <robvcode@gmail.com>
2014-08-31 23:16:37 +0200

    Isolated model mapping in separate classes

---
+++ JungleControls/FacadeControls/FacadeInstance.cs
@@ -0,0 +1,61 @@
+﻿using Assisticant;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace JungleControls
+{
+    public class FacadeInstance
+    {
+        readonly FacadeType FacadeType;
+        readonly DependencyObject Facade;
+        object Model;
+        FacadeProperty[] ModelFields;
+
+        public FacadeInstance(FacadeType facadeType, DependencyObject facade)
+        {
+            FacadeType = facadeType;
+            Facade = facade;
+        }
+
+        public void ApplyModel(DependencyObject obj)
+        {
+            var element = obj as FrameworkElement;
+            if (element != null)
+                element.DataContext = ForView.Wrap(GetModel());
+        }
+
+        public object GetModel()
+        {
+            if (Model == null)
+            {
+                Model = Activator.CreateInstance(FacadeType.ModelType);
+                ModelFields = new FacadeProperty[FacadeType.FieldMappings.Count];
+                foreach (var mapping in FacadeType.FieldMappings)
+                {
+                    var computed = Activator.CreateInstance(typeof(FacadeProperty<>).MakeGenericType(mapping.FacadeProperty.PropertyType), Facade, mapping.FacadeProperty);
+                    ModelFields[mapping.PropertyMetadata.Index] = (FacadeProperty)computed;
+                    mapping.ModelField.SetValue(Model, computed);
+                }
+            }
+            return Model;
+        }
+
+        internal void NotifyModel(object sender, DependencyPropertyChangedEventArgs args)
+        {
+            if (ModelFields != null)
+            {
+                if (sender != Facade)
+                    throw new InvalidOperationException("Misrouted property change notification");
+                var metadata = args.Property.GetMetadata(Facade) as FacadePropertyMetadata;
+                if (metadata == null)
+                    throw new InvalidOperationException("Received for notification that doesn't have FacadePropertyMetadata: " + args.Property);
+                var computed = ModelFields[metadata.Index];
+                computed.Invalidate();
+            }
+        }
+    }
+}
---
+++ JungleControls/FacadeControls/FacadeType.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace JungleControls
+{
+    public class FacadeType
+    {
+        internal readonly Type ComponentType;
+        internal readonly Type ModelType;
+        internal readonly List<FacadeMapping> FieldMappings = new List<FacadeMapping>();
+
+        public FacadeType(Type facadeType, Type modelType)
+        {
+            ComponentType = facadeType;
+            ModelType = modelType;
+            var properties = (from field in ComponentType.GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)
+                              where field.FieldType == typeof(DependencyProperty) && field.Name.EndsWith("Property")
+                              select (DependencyProperty)field.GetValue(null)).ToList();
+            foreach (var field in ModelType.GetFields().Where(f => f.FieldType.IsGenericType && f.FieldType.GetGenericTypeDefinition() == typeof(FacadeProperty<>)))
+            {
+                var property = properties.FirstOrDefault(p => p.Name == field.Name);
+                if (property == null)
+                    throw new InvalidOperationException("No property corresponds to FacadeProperty: " + field.Name);
+                if (field.FieldType.GenericTypeArguments[0] != property.PropertyType)
+                    throw new InvalidOperationException("FacadeProperty associated to dependency property has different value type: " + field.Name);
+                var metadata = property.GetMetadata(ComponentType) as FacadePropertyMetadata;
+                if (metadata == null)
+                {
+                    if (property.OwnerType == ComponentType)
+                        throw new InvalidOperationException("FacadePropertyMetadata must be used for FacadeProperty: " + field.Name);
+                    property.OverrideMetadata(ComponentType, metadata = new FacadePropertyMetadata());
+                }
+                metadata.Index = FieldMappings.Count;
+                FieldMappings.Add(new FacadeMapping(field, property, metadata));
+            }
+        }
+
+        internal static void NotifyModel(object sender, DependencyPropertyChangedEventArgs args)
+        {
+            var obj = sender as IFacadeObject;
+            if (obj == null)
+                throw new InvalidOperationException("Facade does not implement IFacadeObject: " + sender);
+            obj.FacadeInstance.NotifyModel(sender, args);
+        }
+    }
+}
