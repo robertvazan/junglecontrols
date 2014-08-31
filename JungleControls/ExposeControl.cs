cf6f3833ef6f172537d4950e5bb3b56bd024ffbf
Robert Va?an <Robert Va?an@localhost>
2014-08-31 02:26:44 +0200

    Added ExposeControl

---
+++ JungleControls/ExposeControl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace JungleControls
+{
+    public static class ExposeControl
+    {
+        static readonly object NoObject = new object();
+
+        public static readonly DependencyProperty AsProperty = DependencyProperty.RegisterAttached("As", typeof(object), typeof(ExposeControl),
+            new FrameworkPropertyMetadata(NoObject, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, AsChanged));
+        public static object GetAs(DependencyObject obj) { return obj.GetValue(AsProperty); }
+        public static void SetAs(DependencyObject obj, object value) { obj.SetValue(AsProperty, value); }
+
+        static void AsChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        {
+            if (args.NewValue != obj)
+                obj.Dispatcher.BeginInvoke(() => obj.SetCurrentValue(AsProperty, obj));
+        }
+    }
+}
--- TestApp/DesignViewModelLocator.cs
+++ TestApp/DesignViewModelLocator.cs
@@ -13,5 +13,6 @@ namespace TestApp
         public object PropertySheet { get { return ViewModel(() => new PropertySheetModel()); } }
         public object DelayedContentControl { get { return ViewModel(() => new DelayedContentControlModel()); } }
         public object LazyControl { get { return ViewModel(() => new LazyControlModel()); } }
+        public object ExposeControl { get { return ViewModel(() => new ExposeControlModel()); } }
     }
 }
