6c589eab27536db1751ae6bcd05090aa0a1d9b3c
Robert Vazan <robvcode@gmail.com>
2014-08-31 02:26:44 +0200

    Added ExposeControl

---
+++ TestApp/ExposeControlSample/ExposeControlModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using UpdateControls.XAML;
+
+namespace TestApp
+{
+    class ExposeControlModel
+    {
+        public TextBlock ScrollMeAway { get; set; }
+        public ICommand ClickMe { get { return MakeCommand.Do(DoClickMe); } }
+
+        public void DoClickMe()
+        {
+            ScrollMeAway.BringIntoView();
+        }
+    }
+}
