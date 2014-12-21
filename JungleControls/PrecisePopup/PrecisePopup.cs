a63e8230978affdaaed020e833ed24a3d59db8dd
Robert Va?an <Robert Va?an@localhost>
2014-12-21 14:08:11 +0100

    Added PrecisePopup control

---
+++ JungleControls/PrecisePopup/PrecisePopup.cs
@@ -0,0 +1,57 @@
+﻿using Assisticant.Collections;
+using Assisticant.Facades;
+using AutoDependencyPropertyMarker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace JungleControls
+{
+    [AutoDependencyProperty]
+    public class PrecisePopup : ContentControl
+    {
+        readonly PrecisePopupModel Model;
+
+        public FrameworkElement PlacementTarget { get; set; }
+        public FrameworkElement SuppressTarget { get; set; }
+        [AutoDependencyProperty(Options = FrameworkPropertyMetadataOptions.BindsTwoWayByDefault)]
+        public bool IsOpen { get; set; }
+        public bool AllowsTransparency { get; set; }
+
+        static readonly DependencyPropertyKey SelectedPlacementKey = DependencyProperty.RegisterReadOnly("SelectedPlacement", typeof(PrecisePopupPlacement), typeof(PrecisePopup), new FrameworkPropertyMetadata());
+        public static readonly DependencyProperty SelectedPlacementProperty = SelectedPlacementKey.DependencyProperty;
+        public PrecisePopupPlacement SelectedPlacement { get { return (PrecisePopupPlacement)GetValue(SelectedPlacementProperty); } }
+
+        static readonly DependencyPropertyKey PlacementsKey = DependencyProperty.RegisterReadOnly("Placements", typeof(PrecisePopupPlacements), typeof(PrecisePopup), new FrameworkPropertyMetadata());
+        public static readonly DependencyProperty PlacementsProperty = PlacementsKey.DependencyProperty;
+        public PrecisePopupPlacements Placements { get { return (PrecisePopupPlacements)GetValue(PlacementsProperty); } }
+
+        static PrecisePopup()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(PrecisePopup), new FrameworkPropertyMetadata(typeof(PrecisePopup)));
+            VisibilityProperty.OverrideMetadata(typeof(PrecisePopup), new FrameworkPropertyMetadata(Visibility.Collapsed));
+        }
+
+        public PrecisePopup()
+        {
+            Model = new PrecisePopupModel(this);
+            SetValue(PlacementsKey, new PrecisePopupPlacements(this));
+            FacadeModel.UpdateAll(Model, this);
+        }
+
+        protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs e)
+        {
+            FacadeModel.Update(Model, this, e);
+            base.OnPropertyChanged(e);
+        }
+
+        internal void AddPlacement(PrecisePopupPlacement placement) { AddLogicalChild(placement); }
+        internal void RemovePlacement(PrecisePopupPlacement placement) { RemoveLogicalChild(placement); }
+        internal void UpdateSelectedPlacement() { SetValue(SelectedPlacementKey, Model.IsOpen ? Model.SelectedCandidate.Placement.PlacementControl : null); }
+    }
+}
---
+++ JungleControls/PrecisePopup/PrecisePopupWindow.cs
@@ -0,0 +1,119 @@
+﻿using Assisticant;
+using Assisticant.Facades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace JungleControls
+{
+    class PrecisePopupWindow : Window
+    {
+        readonly PrecisePopupModel Model;
+        readonly ComputedJob PositioningJob;
+        bool IsHidden;
+
+        static PrecisePopupWindow()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(PrecisePopupWindow), new FrameworkPropertyMetadata(typeof(PrecisePopupWindow)));
+            TopmostProperty.OverrideMetadata(typeof(PrecisePopupWindow), new FrameworkPropertyMetadata(true));
+            WindowStyleProperty.OverrideMetadata(typeof(PrecisePopupWindow), new FrameworkPropertyMetadata(WindowStyle.None));
+            ResizeModeProperty.OverrideMetadata(typeof(PrecisePopupWindow), new FrameworkPropertyMetadata(ResizeMode.NoResize));
+            SizeToContentProperty.OverrideMetadata(typeof(PrecisePopupWindow), new FrameworkPropertyMetadata(SizeToContent.WidthAndHeight));
+            ShowInTaskbarProperty.OverrideMetadata(typeof(PrecisePopupWindow), new FrameworkPropertyMetadata(false));
+            AllowsTransparencyProperty.OverrideMetadata(typeof(PrecisePopupWindow), new FrameworkPropertyMetadata(true));
+        }
+
+        internal PrecisePopupWindow(PrecisePopupModel model)
+        {
+            Model = model;
+            DataContext = FacadeModel.Wrap(Model);
+            PositioningJob = new ComputedJob(() =>
+            {
+                Left = Model.SelectedCandidate.X;
+                Top = Model.SelectedCandidate.Y;
+                Model.PopupControl.UpdateSelectedPlacement();
+            });
+            PositioningJob.Start();
+            Loaded += (s, args) => Mouse.Capture(this, CaptureMode.SubTree);
+        }
+
+        protected override void OnPreviewMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            OnPreviewMouseButton(e);
+            base.OnPreviewMouseLeftButtonDown(e);
+        }
+
+        protected override void OnPreviewMouseLeftButtonUp(MouseButtonEventArgs e)
+        {
+            OnPreviewMouseButton(e);
+            base.OnPreviewMouseLeftButtonUp(e);
+        }
+
+        protected override void OnPreviewMouseRightButtonDown(MouseButtonEventArgs e)
+        {
+            OnPreviewMouseButton(e);
+            base.OnPreviewMouseRightButtonDown(e);
+        }
+
+        protected override void OnPreviewMouseRightButtonUp(MouseButtonEventArgs e)
+        {
+            OnPreviewMouseButton(e);
+            base.OnPreviewMouseRightButtonUp(e);
+        }
+
+        void OnPreviewMouseButton(MouseButtonEventArgs e)
+        {
+            if (InputHitTest(e.GetPosition(this)) == null)
+            {
+                if (Model.SuppressTarget.Value.InputHitTest(e.GetPosition(Model.SuppressTarget.Value)) != null)
+                    e.Handled = true;
+                Model.PopupControl.IsOpen = false;
+            }
+        }
+
+        protected override void OnDeactivated(EventArgs e)
+        {
+            Model.PopupControl.IsOpen = false;
+            base.OnDeactivated(e);
+        }
+
+        protected override void OnLostMouseCapture(MouseEventArgs e)
+        {
+            base.OnLostMouseCapture(e);
+            if (Mouse.Captured == null && !IsHidden)
+                Mouse.Capture(this, CaptureMode.SubTree);
+        }
+
+        public void Shutdown()
+        {
+            IsHidden = true;
+            PositioningJob.Stop();
+            if (Mouse.Captured == this)
+                Mouse.Capture(null);
+            Hide();
+            Dispatcher.BeginInvoke(new Action(() => Close()), DispatcherPriority.Input);
+        }
+
+        protected override void OnLocationChanged(EventArgs e)
+        {
+            base.OnLocationChanged(e);
+        }
+
+        protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
+        {
+            base.OnRenderSizeChanged(sizeInfo);
+            if (Math.Abs(Model.PopupWidth.Value - sizeInfo.NewSize.Width) > 0.001 || Math.Abs(Model.PopupHeight.Value - sizeInfo.NewSize.Height) > 0.001)
+            {
+                Model.PopupWidth.Value = sizeInfo.NewSize.Width;
+                Model.PopupHeight.Value = sizeInfo.NewSize.Height;
+                Model.WindowVisibility.Value = Visibility.Visible;
+            }
+        }
+    }
+}
